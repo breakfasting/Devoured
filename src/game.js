653fa9dbdbfc6d017214a58cb8f4a0fd12fb4404
breakfasting <breakfast.ing@gmail.com>
2020-05-19 00:19:35 -0700

    testing tiles

--- src/game.js
+++ src/game.js
@@ -9,20 +9,49 @@ class Game {
       width: window.innerWidth,
       height: window.innerHeight,
     });
+
+    this.loader = PIXI.Loader.shared;
+    this.buildTile = this.buildTile.bind(this);
+    this.tileSize = 32;
+    this.map = {
+      width: 4,
+      height: 4,
+      tiles: [],
+    }
+  }
+
+  load() {
+    this.loader.add('tileset', 'assets/tiles.png')
+      .on('progress', (loader) => {
+        console.log(`${loader.progress}% loaded`);
+      })
+      .load(this.buildTile);
+  }
+
+  buildTile() {
+    const tileset = this.loader.resources.tileset.texture;
+    const tileTextures = [];
+    for (let i = 0; i < 32 * 63; i += 1) {
+      const x = i % 32;
+      const y = Math.floor(i / 32);
+      tileTextures[i] = new PIXI.Texture(
+        tileset,
+        new PIXI.Rectangle(x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize),
+      );
+    }
+    this.img = new PIXI.Sprite(tileTextures[355]);
+    this.start();
   }
 
   start() {
-    const texture = PIXI.Texture.from('assets/test.png');
-    const img = new PIXI.Sprite(texture);
-
-    img.x = this.app.renderer.width / 2;
-    img.y = this.app.renderer.height / 2;
-    img.anchor.set(0.5);
-
-    this.app.stage.addChild(img);
+    this.img.scale.set(2);
+    this.img.x = this.app.renderer.width / 2;
+    this.img.y = this.app.renderer.height / 2;
+    this.img.anchor.set(0.5);
+    this.app.stage.addChild(this.img);
 
     this.app.ticker.add(() => {
-      img.rotation += 0.1;
+      this.img.rotation += 0.01;
     });
     this.app.ticker.start();
   }
