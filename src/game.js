34fb75638757ff890bb02969924ef06c1d9f1ab5
breakfasting <breakfast.ing@gmail.com>
2020-05-19 22:16:36 -0700

    render json map in game

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import * as map from './kitchen.json';
 
 class Game {
   constructor() {
@@ -12,12 +13,11 @@ class Game {
 
     this.loader = PIXI.Loader.shared;
     this.buildTile = this.buildTile.bind(this);
-    this.tileSize = 32;
-    this.map = {
-      width: 4,
-      height: 4,
-      tiles: [],
-    }
+    this.tileSize = 16;
+    this.map = map.default;
+    this.tileTextures = [];
+    this.layers = [];
+    // console.log(map.default);
   }
 
   load() {
@@ -30,16 +30,35 @@ class Game {
 
   buildTile() {
     const tileset = this.loader.resources.tileset.texture;
-    const tileTextures = [];
-    for (let i = 0; i < 32 * 63; i += 1) {
-      const x = i % 32;
-      const y = Math.floor(i / 32);
-      tileTextures[i] = new PIXI.Texture(
+    for (let i = 0; i < 64 * 126; i += 1) {
+      const x = i % 64;
+      const y = Math.floor(i / 64);
+      this.tileTextures[i] = new PIXI.Texture(
         tileset,
         new PIXI.Rectangle(x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize),
       );
     }
-    this.img = new PIXI.Sprite(tileTextures[355]);
+    this.img = new PIXI.Sprite(this.tileTextures[333]);
+    this.testBuildMap();
+  }
+
+  testBuildMap() {
+    for (let layer = 0; layer < this.map.layers.length; layer += 1) {
+      const oneLayer = new PIXI.Container();
+      for (let y = 0; y < this.map.width; y += 1) {
+        for (let x = 0; x < this.map.width; x += 1) {
+          const tile = this.map.layers[layer].data[y * this.map.width + x];
+          if (tile !== 0) {
+            const sprite = new PIXI.Sprite(this.tileTextures[tile - 1]);
+            sprite.x = x * this.tileSize;
+            sprite.y = y * this.tileSize;
+            oneLayer.addChild(sprite);
+          }
+        }
+      }
+      oneLayer.scale.set(2);
+      this.layers.push(oneLayer);
+    }
     this.start();
   }
 
@@ -48,7 +67,9 @@ class Game {
     this.img.x = this.app.renderer.width / 2;
     this.img.y = this.app.renderer.height / 2;
     this.img.anchor.set(0.5);
-    this.app.stage.addChild(this.img);
+    for (let layer of this.layers) {
+      this.app.stage.addChild(layer);
+    }
 
     this.app.ticker.add(() => {
       this.img.rotation += 0.01;
